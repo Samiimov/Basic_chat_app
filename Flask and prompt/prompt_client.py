feb7c6f1c512a3b58bb997fd29274b8717a1b9e4
SamiPeltola <sami.peltola@qautomate.fi>
2022-04-30 14:09:33 +0300

    removed prompt colors

--- Flask and prompt/prompt_client.py
+++ Flask and prompt/prompt_client.py
@@ -8,6 +8,10 @@ import sys
 def listen_for_messages():
     while True:
         message = s.recv(1024).decode()
+        if "<COLOR>" in message:
+            message_list = message.split("<COLOR>")
+            message = ""
+            message = message_list[0]
         print("\n" + message)
 
 def send_messages(name : str):
@@ -18,27 +22,15 @@ def send_messages(name : str):
             # a way to exit the program
             if to_send.lower() == 'q':
                 break
-            # add the datetime, name & the color of the sender
+            # add the datetime, name & the sender
             date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-            to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+            to_send = f"[{date_now}] {name}{separator_token}{to_send}"
             # finally, send the message
             s.send(to_send.encode())
         except KeyboardInterrupt:
             sys.exit()
 
 if __name__ == "__main__":
-    # init colors
-    init()
-
-    # set the available colors
-    colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-        Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-        Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-        Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
-    ]
-
-    # choose a random color for the client
-    client_color = random.choice(colors)
 
     # server's IP address
     # if the server is not on this machine, 
@@ -62,6 +54,6 @@ if __name__ == "__main__":
     t.start()
     name = input("Given username: ")
     date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-    to_send = f"{client_color}[{date_now}] <NAME> {name}"
+    to_send = f"[{date_now}] <NAME> {name}"
     s.send(to_send.encode())
     send_messages(name)
